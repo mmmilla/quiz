83b68e157d8acb0bae6d8b86b750ca2f66305b00
manuel molino <push.default>
2015-06-06 12:54:50 +0200

    Añadido GET author

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -7,3 +7,6 @@ exports.answer = function(req,res) {
     else
 	res.render('quizes/answer', {respuesta: 'Incorrecto'});
 };
+exports.author = function(req,res) {
+	res.render('quizes/author', {autor: 'Manuel J. Molino'});
+};
