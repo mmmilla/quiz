2cdc575af48bea64626a7fc56b34019ae4405816
manuel molino <push.default>
2015-06-09 21:13:15 +0200

    Primer intento de listas de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,9 +1,50 @@
-exports.question = function(req,res) {
-	res.render('quizes/question', {pregunta: 'Capital de Italia'});
+var models = require('../models/models.js');
+/*exports.question = function(req,res) {
+	models.Quiz.findAll().success(function(quiz) {
+	res.render('quizes/question', {pregunta: quiz[0].pregunta});
+	});
 };
 exports.answer = function(req,res) {
-    if (req.query.respuesta === 'Roma')
+    models.Quiz.findAll().success(function(quiz) {
+    if (req.query.respuesta === quiz[0].respuesta)
 	res.render('quizes/answer', {respuesta: 'Correcto'});
     else
 	res.render('quizes/answer', {respuesta: 'Incorrecto'});
+    });
+};*/
+
+// Autoload :id
+exports.load = function(req, res, next, quizId) {
+  models.Quiz.find(quizId).then(
+	function(quiz) {
+           if (quiz) {
+              req.quiz = quiz;
+              next();
+           } else{next(new Error('No existe quizId=' + quizId));}
+    }
+  ).catch(function(error){next(error);});
+};
+
+// GET /quizes
+// GET /users/:userId/quizes
+exports.index = function(req, res) {  
+  models.Quiz.findAll().then(
+    function(quizes) {
+      res.render('quizes/index', {quizes: quizes});
+    }
+  ).catch(function(error){next(error)});
+};
+
+// GET /quizes/:id
+exports.show = function(req, res) {
+  res.render('quizes/show', { quiz: quiz});
+};            // req.quiz: instancia de quiz cargada con autoload
+
+// GET /quizes/:id/answer
+exports.answer = function(req, res) {
+  var resultado = 'Incorrecto';
+  if (req.query.respuesta === req.quiz.respuesta) {
+    resultado = 'Correcto';
+  }
+  res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado});
 };
--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,15 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+// Autoload de comandos con ids
+router.param('quizId', quizController.load);  // autoload :quizId
+
+// Definición de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+//router.get('/quizes/question', quizController.question);
+//router.get('/quizes/answer', quizController.answer);
 router.get('/author', function(req, res) {
    res.render('author', {autor: 'Manuel J. Molino'});
 });
